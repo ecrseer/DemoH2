bba77c4ea2e001f5c30be21da74ddff9148e4c41
Gabriel J <60169516+ecrseer@users.noreply.github.com>
2020-07-24 21:03:49 -0300

    autowired
    lembrar de ler autowired

--- src/main/java/br/com/novoAgadois/demoDoAgaDois/Controles/RepoPessoa.java
+++ src/main/java/br/com/novoAgadois/demoDoAgaDois/Controles/RepoPessoa.java
@@ -3,5 +3,5 @@ package br.com.novoAgadois.demoDoAgaDois.Controles;
 import br.com.novoAgadois.demoDoAgaDois.Muitici.Pessoa;
 import org.springframework.data.repository.CrudRepository;
 
-public interface RepoPessoa extends CrudRepository<Pessoa,int> {
+public interface RepoPessoa extends CrudRepository<Pessoa,Integer> {
 }
